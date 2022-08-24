d167de51029142ad476f7e97c90fe565f122b958
Aaron Trowbridge <aaron@pop-os.localdomain>
2022-08-24 18:30:17 -0400

    hacky submodules intall script

--- .config_scripts/initsubmodules.sh
+++ .config_scripts/initsubmodules.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+
+# change dir to home to add first layer of submodules
+
+cd $HOME
+
 set -e
 
 git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
@@ -7,7 +12,26 @@ git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
     do
         url_key=$(echo $path_key | sed 's/\.path/.url/')
         url=$(git config -f .gitmodules --get "$url_key")
-        git submodule add $url $local_path
+        git submodule add -f $url $local_path
     done
+
+# change dir to .oh-my-zsh
+
+cd -
+
+cd ~/.oh-my-zsh
+
+set -e
+
+git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
+    while read path_key local_path
+    do
+        url_key=$(echo $path_key | sed 's/\.path/.url/')
+        url=$(git config -f .gitmodules --get "$url_key")
+	rm -rf $local_path
+        git submodule add -f $url $local_path
+    done
+
+cd -
 
 
